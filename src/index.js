2277b12f908707755b5b158cc1959c008c978435
LudvigThunberg <ludvigthunberg@gmail.com>
2022-02-28 13:38:13 +0100

    Added login, logout and forceAuthorize

--- src/index.js
+++ src/index.js
@@ -17,12 +17,12 @@ const app = express();
 // ======= CONFIG =======
 new customMorgan(morgan, app).enable(); // Custom morgan
 app.engine(
-    // Configure engine.
-    "hbs",
-    expressHandlebars.engine({
-        extname: ".hbs",
-        defaultLayout: "main",
-    })
+  // Configure engine.
+  "hbs",
+  expressHandlebars.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
 );
 app.set("view engine", "hbs"); // Tell which engine to set / use.
 app.set("views", path.join(__dirname, "views")); // Tell where the views folder is located.
@@ -35,14 +35,56 @@ app.use("/assets", express.static(path.join(__dirname, "./public")));
 // ======= ROUTERS =======
 app.use("/recipes", recipesRouter);
 // ======= ROUTES =======
+
+// Auth Middleware
+app.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    const tokenData = jwt.decode(token, process.env.JWTSECRET);
+    res.locals.loggedIn = true;
+    res.locals.username = tokenData.username;
+    res.locals.userId = tokenData.userId;
+  } else {
+    res.locals.loggedIn = false;
+  }
+  next();
+});
+
 // Home
 app.get("/", (req, res) => {
-    res.render("home", {
-        title: "Home",
-    });
+  res.render("home", {
+    title: "Home",
+  });
+});
+
+// Login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  UsersModel.findOne({ username }, (err, user) => {
+    if (user && utils.comparePassword(password, user.hashedPassword)) {
+      const userData = {
+        user: user._id,
+        username,
+      };
+      const accessToken = jwt.sign(userData, process.env.JWTSECRET);
+
+      res.cookie("token", accessToken);
+      res.redirect("/");
+    } else {
+      res.status(400).send("login Failed");
+    }
+  });
+});
+
+// Logout
+app.post("/logout", (req, res) => {
+  res.cookie("token", "", { maxAge: 0 });
+  res.redirect("/");
 });
 
 // ======= LISTEN =======
 app.listen(8000, () => {
-    console.log("http://localhost:8000/");
+  console.log("http://localhost:8000/");
 });
--- src/utils.js
+++ src/utils.js
@@ -1,62 +1,77 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const INGREDIENT_UNITS = ["st", "ml", "cl", "dl", "l", "mg", "g", "hg", "kg"];
 const INGREDIENT_CATEGORIES = [
-    "kött",
-    "fisk",
-    "frukt",
-    "bär",
-    "mjölk",
-    "laktos",
+  "kött",
+  "fisk",
+  "frukt",
+  "bär",
+  "mjölk",
+  "laktos",
 ];
 
 const validateRecipe = (name, chefId, description, image, ingredients) => {
-    if (typeof name !== "string" || name.length <= 0) throw "Invalid Name";
-    // TEMPORARY
-    chefId = new mongoose.Types.ObjectId();
-    // ===
-    if (!mongoose.Types.ObjectId.isValid(chefId)) throw "Invalid ID";
-    if (typeof description !== "string" || description.length <= 0)
-        throw "Invalid description";
-    // Validation of image here (If we implement it).
-    if (!Array.isArray(ingredients)) throw "Invalid ingredients";
-    ingredients.forEach((entry) => {
-        // Check all nessecary properties exists.
-        if (!("ingredient" in entry)) throw "Missing ingredient name";
-        if (!("amount" in entry)) throw "Missing ingredient amount";
-        if (!("unit" in entry)) throw "Missing ingredient unit";
-        if (!("categories" in entry)) throw "Missing ingredient categories";
+  if (typeof name !== "string" || name.length <= 0) throw "Invalid Name";
+  // TEMPORARY
+  chefId = new mongoose.Types.ObjectId();
+  // ===
+  if (!mongoose.Types.ObjectId.isValid(chefId)) throw "Invalid ID";
+  if (typeof description !== "string" || description.length <= 0)
+    throw "Invalid description";
+  // Validation of image here (If we implement it).
+  if (!Array.isArray(ingredients)) throw "Invalid ingredients";
+  ingredients.forEach((entry) => {
+    // Check all nessecary properties exists.
+    if (!("ingredient" in entry)) throw "Missing ingredient name";
+    if (!("amount" in entry)) throw "Missing ingredient amount";
+    if (!("unit" in entry)) throw "Missing ingredient unit";
+    if (!("categories" in entry)) throw "Missing ingredient categories";
 
-        // Check if properties are of expected type and contains expected value(s);
-        // === Ingredient (name) ===
-        if (
-            typeof entry.ingredient !== "string" ||
-            entry.ingredient.length <= 0
-        )
-            throw "Invalid ingredient name";
-        // === Amount ===
-        if (typeof entry.amount !== "number" || entry.amount <= 0)
-            throw "Invalid ingredient amount";
-        // === Unit ===
-        if (
-            typeof entry.unit !== "string" ||
-            !INGREDIENT_UNITS.includes(entry.unit.toLowerCase())
-        )
-            throw "Invalid ingredient unit";
-        // === Categories ===
-        if (!Array.isArray(entry.categories))
-            throw "Invalid ingredient categories";
-        entry.categories.forEach((category) => {
-            if (
-                typeof category !== "string" ||
-                !INGREDIENT_CATEGORIES.includes(category.toLowerCase())
-            )
-                throw "Invalid category";
-        });
+    // Check if properties are of expected type and contains expected value(s);
+    // === Ingredient (name) ===
+    if (typeof entry.ingredient !== "string" || entry.ingredient.length <= 0)
+      throw "Invalid ingredient name";
+    // === Amount ===
+    if (typeof entry.amount !== "number" || entry.amount <= 0)
+      throw "Invalid ingredient amount";
+    // === Unit ===
+    if (
+      typeof entry.unit !== "string" ||
+      !INGREDIENT_UNITS.includes(entry.unit.toLowerCase())
+    )
+      throw "Invalid ingredient unit";
+    // === Categories ===
+    if (!Array.isArray(entry.categories)) throw "Invalid ingredient categories";
+    entry.categories.forEach((category) => {
+      if (
+        typeof category !== "string" ||
+        !INGREDIENT_CATEGORIES.includes(category.toLowerCase())
+      )
+        throw "Invalid category";
     });
+  });
+};
+
+const comparePassword = (password, hash) => {
+  const correct = bcrypt.compareSync(password, hash);
+  return correct;
+};
+
+const forceAuthorize = (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token && jwt.verify(token, process.env.JWTSECRET)) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
 };
 
 module.exports = {
-    INGREDIENT_UNITS,
-    validateRecipe,
+  INGREDIENT_UNITS,
+  validateRecipe,
+  comparePassword,
+  forceAuthorize,
 };
