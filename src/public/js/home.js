991e64016168089d204761996b8a7575bc8be3fb
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-03-02 14:56:47 +0100

    Fixed search icon bug for home.html

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,10 +1,11 @@
 window.onload = () => {
     let searchBar = document.getElementById("search-bar");
     let searchIcon = document.getElementById("search-icon");
-    searchBar.addEventListener("focusin", () => {
-        searchIcon.style.display = "none";
-    });
-    searchBar.addEventListener("focusout", () => {
-        searchIcon.style.display = "flex";
+
+    if (searchBar.value.length <= 0) searchIcon.style.display = "flex";
+
+    searchBar.addEventListener("input", () => {
+        if (searchBar.value.length <= 0) searchIcon.style.display = "flex";
+        else searchIcon.style.display = "none";
     });
 };
