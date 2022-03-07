bd7b6f555f99afb4ce22849607e66a0cf3cef9fb
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-03-07 16:12:52 +0100

    Added overflow-x hidden to body

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,12 +1,9 @@
 window.onload = () => {
     const rawData = document.getElementById("js-data").getAttribute("data-js");
-
     if (rawData) {
         const jsData = JSON.parse(rawData);
         loadScripts(jsData);
     }
-
-    initializeTransition();
 };
 
 function loadScripts(urls) {
@@ -19,5 +16,3 @@ function loadScripts(urls) {
         head.appendChild(script);
     });
 }
-
-function initializeTransition() {}
