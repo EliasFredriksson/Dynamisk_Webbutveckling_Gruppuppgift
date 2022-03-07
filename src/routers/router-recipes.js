f8e256bc4fbefdf40337159900ad3c627de02722
LudvigThunberg <ludvigthunberg@gmail.com>
2022-03-07 11:01:14 +0100

    fixed merge issues

--- src/routers/router-recipes.js
+++ src/routers/router-recipes.js
@@ -3,11 +3,11 @@ const express = require("express");
 const settings = require("../settings");
 
 const {
-    validateRecipe,
-    validateComment,
-    forceAuthorize,
-    forceOwnComment,
-    forceOwnRecipe,
+  validateRecipe,
+  validateComment,
+  forceAuthorize,
+  forceOwnComment,
+  forceOwnRecipe,
 } = require("../utils");
 const RecipesModel = require("../models/RecipesModel.js");
 const UsersModel = require("../models/UsersModels.js");
@@ -20,226 +20,208 @@ const recipesRouter = express.Router();
 // ####################### CREATE #######################
 // Go to create recipe page.
 recipesRouter.get("/create", forceAuthorize, (req, res) => {
-    // res.status(200).send("Sent back to create page.");
-    res.render("recipes-create", {
-        title: "Create Recipe",
-        ingredientUnits: settings.INGREDIENT_UNITS,
-        ingredientCategories: settings.INGREDIENT_CATEGORIES,
-    });
+  // res.status(200).send("Sent back to create page.");
+  res.render("recipes-create", {
+    title: "Create Recipe",
+    ingredientUnits: settings.INGREDIENT_UNITS,
+    ingredientCategories: settings.INGREDIENT_CATEGORIES,
+  });
 });
 
 // Create new recipe and save to database.
 // MISSING:
 //      Adding the recipe to the currently logged in users recipe.
 recipesRouter.post("/create", forceAuthorize, async (req, res) => {
-    const { name, description, image, ingredients } = req.body;
-    try {
-        if (!res.locals.loggedIn) throw "Not logged in.";
-        validateRecipe(
-            name,
-            res.locals.id,
-            description,
-            image,
-            ingredients,
-            []
-        );
-        const newRecipe = new RecipesModel({
-            name: name,
-            chef: new mongoose.Types.ObjectId(res.locals.id),
-            description: description,
-            image: "IMAGE_PLACEHOLDER",
-            ingredients: ingredients,
-            comments: [],
-        });
+  const { name, description, image, ingredients } = req.body;
+  try {
+    if (!res.locals.loggedIn) throw "Not logged in.";
+    validateRecipe(name, res.locals.id, description, image, ingredients, []);
+    const newRecipe = new RecipesModel({
+      name: name,
+      chef: new mongoose.Types.ObjectId(res.locals.id),
+      description: description,
+      image: "IMAGE_PLACEHOLDER",
+      ingredients: ingredients,
+      comments: [],
+    });
 
-        await newRecipe.save();
-        res.status(201).send(newRecipe._id);
-    } catch (error) {
-        console.log(
-            "\n\n================= ERROR =================\n",
-            error,
-            "\n\n"
-        );
-        res.status(400).send(error);
-    }
+    await newRecipe.save();
+    res.status(201).send(newRecipe._id);
+  } catch (error) {
+    console.log(
+      "\n\n================= ERROR =================\n",
+      error,
+      "\n\n"
+    );
+    res.status(400).send(error);
+  }
 });
 // ######################## READ ########################
 // Get recipe by provided ID. Send found recipe to/and render recipe single page.
 recipesRouter.get("/:id", async (req, res) => {
-    try {
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) throw "Invalid Id";
-        const recipe = await RecipesModel.findById(req.params.id)
-            .populate("comments.comment")
-            .lean();
-        const chef = await UsersModel.findById(recipe.chef).lean();
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) throw "Invalid Id";
+    const recipe = await RecipesModel.findById(req.params.id)
+      .populate("comments.comment")
+      .lean();
+    const chef = await UsersModel.findById(recipe.chef).lean();
 
-        let recipeCategories = [];
-        recipe.ingredients.forEach((entry) => {
-            if (!recipeCategories.includes(entry.category))
-                recipeCategories.push(entry.category);
-        });
+    let recipeCategories = [];
+    recipe.ingredients.forEach((entry) => {
+      if (!recipeCategories.includes(entry.category))
+        recipeCategories.push(entry.category);
+    });
 
-        res.render("recipes-single", {
-            title: recipe.name,
-            recipe: recipe,
-            chef: chef,
-            categories: recipeCategories,
-        });
-    } catch (error) {
-        console.log("\n\nERROR:", error);
-        res.status(400).redirect("/");
-    }
+    res.render("recipes-single", {
+      title: recipe.name,
+      recipe: recipe,
+      chef: chef,
+      categories: recipeCategories,
+    });
+  } catch (error) {
+    console.log("\n\nERROR:", error);
+    res.status(400).redirect("/");
+  }
 });
 // ######################## UPDATE ########################
 // Go to Update (Edit) recipe page.
 recipesRouter.get("/:id/edit", forceAuthorize, forceOwnRecipe, (req, res) => {
-    RecipesModel.findById(req.params.id, (error, recipe) => {
-        if (error) res.status(500).redirect(`/`);
-        if (recipe) res.status(200).redirect("/");
-        // res.status(200).render("recipe-edit", {
-        //     title: "Edit Recipe",
-        //     recipe: recipe,
-        // });
-        else res.status(400).redirect(`/`);
-    });
+  RecipesModel.findById(req.params.id, (error, recipe) => {
+    if (error) res.status(500).redirect(`/`);
+    if (recipe) res.status(200).redirect("/");
+    // res.status(200).render("recipe-edit", {
+    //     title: "Edit Recipe",
+    //     recipe: recipe,
+    // });
+    else res.status(400).redirect(`/`);
+  });
 });
 // Update recipe and save to database.
 recipesRouter.post("/:id/edit", forceAuthorize, forceOwnRecipe, (req, res) => {
-    const { name, chefId, description, image, ingredients } = req.body;
-    try {
-        validateRecipe(name, chefId, description, image, ingredients, []);
-        RecipesModel.findByIdAndUpdate(
-            req.params.id,
-            {
-                name: name,
-                chef: mongoose.Types.ObjectId(chefId),
-                description: description,
-                image: image,
-                ingredients: ingredients,
-            },
-            (error, result) => {
-                if (error)
-                    res.status(500).redirect(`/recipes/${req.params.id}`);
-                else res.status(200).redirect(`/recipes/${req.params.id}`);
-            }
-        );
-    } catch (error) {
-        console.log("\n\nERROR: ", error);
-        res.status(400).redirect("/");
-    }
+  const { name, chefId, description, image, ingredients } = req.body;
+  try {
+    validateRecipe(name, chefId, description, image, ingredients, []);
+    RecipesModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: name,
+        chef: mongoose.Types.ObjectId(chefId),
+        description: description,
+        image: image,
+        ingredients: ingredients,
+      },
+      (error, result) => {
+        if (error) res.status(500).redirect(`/recipes/${req.params.id}`);
+        else res.status(200).redirect(`/recipes/${req.params.id}`);
+      }
+    );
+  } catch (error) {
+    console.log("\n\nERROR: ", error);
+    res.status(400).redirect("/");
+  }
 });
 // ######################## DELETE ########################
 // Delete recipe
 recipesRouter.post(
-    "/:id/delete",
-    forceAuthorize,
-    forceOwnRecipe,
-    async (req, res) => {
-        RecipesModel.findByIdAndDelete(req.params.id, (error, docs) => {
-            if (error) res.status(500).redirect(`/recipe/${req.params.id}`);
-            else res.status(200).redirect("/");
-        });
-    }
+  "/:id/delete",
+  forceAuthorize,
+  forceOwnRecipe,
+  async (req, res) => {
+    RecipesModel.findByIdAndDelete(req.params.id, (error, docs) => {
+      if (error) res.status(500).redirect(`/recipe/${req.params.id}`);
+      else res.status(200).redirect("/");
+    });
+  }
 );
 
 // ######################## COMMENT ########################
 // Create comment
 recipesRouter.post("/:id/comments/add", forceAuthorize, async (req, res) => {
-    try {
-        if (!res.locals.loggedIn) throw "Not logged in.";
-        validateComment(req.body.text, res.locals.id);
+  try {
+    if (!res.locals.loggedIn) throw "Not logged in.";
+    validateComment(req.body.text, res.locals.id);
 
-        const newComment = new CommentsModel({
-            text: req.body.text,
-            userId: new mongoose.Types.ObjectId(res.locals.id),
-            username: res.locals.username,
-        });
-        const commentResult = await newComment.save();
-        RecipesModel.findByIdAndUpdate(
-            req.params.id,
-            {
-                $push: { comments: { comment: commentResult._id } },
-            },
-            (error, docs, result) => {
-                if (error)
-                    res.status(500).redirect("/recipes/" + req.params.id);
-                else res.status(200).redirect("/recipes/" + req.params.id);
-            }
-        );
-    } catch (error) {
-        console.log(
-            "\n\n================= ERROR =================\n",
-            error,
-            "\n\n"
-        );
-        res.status(400).send(error);
-    }
+    const newComment = new CommentsModel({
+      text: req.body.text,
+      userId: new mongoose.Types.ObjectId(res.locals.id),
+      username: res.locals.username,
+    });
+    const commentResult = await newComment.save();
+    RecipesModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { comments: { comment: commentResult._id } },
+      },
+      (error, docs, result) => {
+        if (error) res.status(500).redirect("/recipes/" + req.params.id);
+        else res.status(200).redirect("/recipes/" + req.params.id);
+      }
+    );
+  } catch (error) {
+    console.log(
+      "\n\n================= ERROR =================\n",
+      error,
+      "\n\n"
+    );
+    res.status(400).send(error);
+  }
 });
 
 // Edit comment
 recipesRouter.post(
-    "/:id/comments/edit/:commentId",
-    forceAuthorize,
-    forceOwnComment,
-    async (req, res) => {
-        try {
-            if (!res.locals.ownComment) throw "Not own comment.";
-            validateComment(req.body.text, req.params.commentId);
-            CommentsModel.findByIdAndUpdate(
-                req.params.commentId,
-                {
-                    text: req.body.text,
-                },
-                (error, docs, result) => {
-                    if (error) res.sendStatus(500);
-                    else res.sendStatus(200);
-                }
-            );
-        } catch (error) {
-            console.log("\n\nERROR: ", error);
-            res.status(400).redirect("/");
+  "/:id/comments/edit/:commentId",
+  forceAuthorize,
+  forceOwnComment,
+  async (req, res) => {
+    try {
+      if (!res.locals.ownComment) throw "Not own comment.";
+      validateComment(req.body.text, req.params.commentId);
+      CommentsModel.findByIdAndUpdate(
+        req.params.commentId,
+        {
+          text: req.body.text,
+        },
+        (error, docs, result) => {
+          if (error) res.sendStatus(500);
+          else res.sendStatus(200);
         }
+      );
+    } catch (error) {
+      console.log("\n\nERROR: ", error);
+      res.status(400).redirect("/");
     }
+  }
 );
 
 // Delete comment
 recipesRouter.post(
-    "/:id/comments/remove/:commentId",
-    forceAuthorize,
-    forceOwnComment,
-    async (req, res) => {
-        RecipesModel.findByIdAndUpdate(
-            req.params.id,
-            {
-                $pull: {
-                    comments: {
-                        comment: new mongoose.Types.ObjectId(
-                            req.params.commentId
-                        ),
-                    },
-                },
-            },
-            (error, docs, result) => {
-                if (error)
-                    res.status(500).redirect("/recipes/" + req.params.id);
-                else {
-                    CommentsModel.findByIdAndDelete(
-                        req.params.commentId,
-                        (error, docs) => {
-                            if (error)
-                                res.status(500).redirect(
-                                    `/recipe/${req.params.id}`
-                                );
-                            else
-                                res.status(200).redirect(
-                                    "/recipes/" + req.params.id
-                                );
-                        }
-                    );
-                }
+  "/:id/comments/remove/:commentId",
+  forceAuthorize,
+  forceOwnComment,
+  async (req, res) => {
+    RecipesModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            comment: new mongoose.Types.ObjectId(req.params.commentId),
+          },
+        },
+      },
+      (error, docs, result) => {
+        if (error) res.status(500).redirect("/recipes/" + req.params.id);
+        else {
+          CommentsModel.findByIdAndDelete(
+            req.params.commentId,
+            (error, docs) => {
+              if (error) res.status(500).redirect(`/recipe/${req.params.id}`);
+              else res.status(200).redirect("/recipes/" + req.params.id);
             }
-        );
-    }
+          );
+        }
+      }
+    );
+  }
 );
 // ########################################################
 
