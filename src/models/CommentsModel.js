57f04774fae03a6d6ac25bc238ada57d699436c6
LudvigThunberg <ludvigthunberg@gmail.com>
2022-03-08 16:51:14 +0100

    added profile picture to comments

--- src/models/CommentsModel.js
+++ src/models/CommentsModel.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const CommentsSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    userId: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-    },
-    username: { type: String, required: true },
+  text: { type: String, required: true },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  username: { type: String, required: true },
+  profileImage: { type: String },
 });
 
 const CommentsModel = mongoose.model("comments", CommentsSchema);
